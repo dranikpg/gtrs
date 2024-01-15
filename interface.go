c79c0aa91dc56d421673561746399b008b9f0765
a <a@tuxpa.in>
2024-01-14 23:30:02 -0600

    alp[habetica

--- interface.go
+++ interface.go
@@ -6,9 +6,9 @@ import "context"
 type Stream[T any] interface {
 	Add(ctx context.Context, v T, idarg ...string) (string, error)
 	Key() string
+	Len(ctx context.Context) (int64, error)
 	Range(ctx context.Context, from, to string, count ...int64) ([]Message[T], error)
 	RevRange(ctx context.Context, from, to string, count ...int64) ([]Message[T], error)
-	Len(ctx context.Context) (int64, error)
 }
 
 // Consumer is a generic consumer interface
